37cc13d9b5fb3eaa2d056e1dfd19734cf4d2de63
Arthur Debarnot <arthur.debarnot@etu.minesparis.psl.eu>
2026-01-30 15:04:06 +0100

    petites modifs include

--- decor.jl
+++ decor.jl
@@ -1,12 +1,10 @@
-include("structures.jl")
-
 # Création des plateformes
 function dessine_moi_une_plateforme(ax,plateforme :: Rectangle)
     poly!(ax, Point2f[plateforme.position,(plateforme.position[1], plateforme.position[2] + plateforme.longueur),(plateforme.position[1] + plateforme.largeur, plateforme.position[2]+plateforme.longueur),(plateforme.position[1]+plateforme.largeur,plateforme.position[2])])
 end
 
 function dessine_moi_un_goo!(ax,goo::Goo)
-    poly!(ax,Circle(Point2f(goo.position[1], goo.position[2]), goo.rayon),color = :blue)
+    poly!(ax, Circle(Point2f(goo.position[1], goo.position[2]), goo.rayon), color=:blue)
 end
 
 function dessine_moi_les_goos!(list_goo)
