8a32508cdb8244a4b7255a1ad91e26a41d3b2612
Elena Drematcheff <elena.drematcheff@etu.minesparis.psl.eu>
2026-01-30 12:39:04 +0100

    fonction dessin lien

--- decor.jl
+++ decor.jl
@@ -1,1 +1,6 @@
 include("structures.jl")
+
+# Création des plateformes
+function dessine_moi_une_plateforme(ax,plateforme :: Rectangle)
+    poly!(ax, Point2f[position,(position[1], position[2] + longueur),(position[1] + largeur, position[2]+longueur),(position[1]+largeur,position[2])])
+end
