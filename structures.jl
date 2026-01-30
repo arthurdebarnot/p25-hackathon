0932a1b423b575b78716cfa11c6b52115b318de9
Arthur Debarnot <arthur.debarnot@etu.minesparis.psl.eu>
2026-01-30 13:42:16 +0100

    changement d'observable

--- structures.jl
+++ structures.jl
@@ -7,10 +7,10 @@ const K = 100u"J/m^2"
 mutable struct Goo
     masse::typeof(u"kg")
     rayon::typeof(u"cm")
-    position::typeof(Observable((0.0u"m",0.0u"m")))
+    position::typeof((0.0u"m",0.0u"m"))
     id::UInt8 # numérote les goos
-    vitesse::typeof(Observable((0.0u"m/s",0.0u"m/s")))
-    forces::typeof(Observable((0.0u"N", 0.0u"N")))
+    vitesse::typeof((0.0u"m/s",0.0u"m/s"))
+    forces::typeof((0.0u"N", 0.0u"N"))
     links = Int[]
 end
 
