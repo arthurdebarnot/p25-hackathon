af39c052cd3a07205b17ef119e4051e12ebd930a
Elena Drematcheff <elena.drematcheff@etu.minesparis.psl.eu>
2026-01-30 13:45:34 +0100

    rangement

--- decor.jl
+++ decor.jl
@@ -4,3 +4,17 @@ include("structures.jl")
 function dessine_moi_une_plateforme(ax,plateforme :: Rectangle)
     poly!(ax, Point2f[position,(position[1], position[2] + longueur),(position[1] + largeur, position[2]+longueur),(position[1]+largeur,position[2])])
 end
+
+function dessine_moi_un_goo(ax,goo::Goo)
+    poly!(ax,Circle(Point2f(goo.position[1], goo.position[2]), goo.rayon),color = :blue)
+end
+
+function dessine_moi_les_goos(list_goo)
+    dessine_moi_un_goo.(list_goo)
+end
+
+function dessine_moi_les_liens(ax,goo ::Goo)
+    for voisin in goo.link
+        lines!(ax,Point2f[goo.position,voisin.position],color = :blue)
+    end
+end
